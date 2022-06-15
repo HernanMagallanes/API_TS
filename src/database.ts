25176b565bc2a63bd9af3add4f03c0a720784e45
Hernán M <hernan.magallanes.94@gmail.com>
2022-06-15 13:24:55 -0300

    env var

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,16 @@
 import { Pool } from "pg";
 
+// import {} from 'dotenv'
+
+require("dotenv").config();
+
+const host = process.env.HOST;
+const password = process.env.PASS;
+
 export const pool = new Pool({
 	user: "postgres",
-	host: "localhost",
-	password: "password2022",
+	host: host,
+	password: password,
 	database: "ts_api_db",
 	port: 5432,
 });
